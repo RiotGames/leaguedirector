5e7b96637fd15bd53c8bceaa814216b5c05aa66a
Cameron Royal <81903+cammm@users.noreply.github.com>
2022-04-05 22:19:11 -0700

    Fix config file search order

--- leaguedirector/enable.py
+++ leaguedirector/enable.py
@@ -82,13 +82,13 @@ def configFilePath(path):
     path = os.path.abspath(path)
     if platform.system() == 'Darwin':
         path = os.path.join(path, 'Contents', 'LoL')
+    config = os.path.join(path, 'DATA', 'CFG', 'game.cfg')
+    if os.path.isfile(config):
+        return config
     config = os.path.join(path, 'Config', 'game.cfg')
     if os.path.isfile(config):
         return config
     config = os.path.join(path, 'Game', 'Config', 'game.cfg')
-    if os.path.isfile(config):
-        return config
-    config = os.path.join(path, 'DATA', 'CFG', 'game.cfg')
     if os.path.isfile(config):
         return config
 
